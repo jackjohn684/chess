490aea02d82b57d895ead006790be1d36df9e8d6
jackjohn684 <jackjohn684@gmail.com>
2024-01-17 16:08:50 -0800

    I added the things from the video

--- shared/src/test/java/passoffTests/chessTests/chessPieceTests/BishopMoveTests.java
+++ shared/src/test/java/passoffTests/chessTests/chessPieceTests/BishopMoveTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 
 import static passoffTests.TestFactory.*;
 
-public class BishopMoveTests {
+public class  BishopMoveTests {
 
     @Test
     public void bishopMoveUntilEdge() {
